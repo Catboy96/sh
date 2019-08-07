68427b9d966c840df797786acb1061fd31bf81ee
Ralf Ren <me@ralf.ren>
2019-08-07 08:18:27 +0800

    Update install.sh

--- easy-screen/install.sh
+++ easy-screen/install.sh
@@ -17,21 +17,18 @@ echo "alias new='screen -S'" >> ~/.bashrc
 echo "" >> ~/.bashrc
 
 echo "Enabling tab complete..."
-sudo cat > /etc/bash_completion.d/goto<<-EOF
-_goto() {
-    local cur prev nodes
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}“
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    screens=$(ls /var/run/screen/S-$USER | grep -oP "(?<=\.).*")
-
-    case $prev in
-        goto)
-            COMPREPLY=( $(compgen -W "${screens}" -- ${cur}) )
-            return;;
-    esac
-} complete -o default -F _goto goto
-EOF
+sudo echo "_goto() {" > /etc/bash_completion.d/goto
+sudo echo "    local cur prev nodes" >> /etc/bash_completion.d/goto
+sudo echo "    COMPREPLY=()" >> /etc/bash_completion.d/goto
+sudo echo "    cur=\"${COMP_WORDS[COMP_CWORD]}\"" >> /etc/bash_completion.d/goto
+sudo echo "    prev=\"${COMP_WORDS[COMP_CWORD-1]}\"" >> /etc/bash_completion.d/goto
+sudo echo "    screens=$(ls /var/run/screen/S-$USER | grep -oP \"(?<=\.).*\")" >> /etc/bash_completion.d/goto
+sudo echo "" >> /etc/bash_completion.d/goto
+sudo echo "    case $prev in" >> /etc/bash_completion.d/goto
+sudo echo "        goto)" >> /etc/bash_completion.d/goto
+sudo echo "            COMPREPLY=( $(compgen -W \"${screens}\" -- ${cur}) )" >> /etc/bash_completion.d/goto
+sudo echo "            return;;" >> /etc/bash_completion.d/goto
+sudo echo "    esac" >> /etc/bash_completion.d/goto
+sudo echo "} complete -o default -F _goto goto" >> /etc/bash_completion.d/goto
 
 echo "Now, run 'source ~/.bashrc' to apply patches."
-
