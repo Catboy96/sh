e041aaf89f7c960153c2a82388db6756fe3c5c5a
Ralf Ren <me@ralf.ren>
2019-08-11 23:18:58 +0800

    Update install.sh

--- easy-screen/install.sh
+++ easy-screen/install.sh
@@ -8,6 +8,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
 
+if [ $UID -ne 0 ]; then
+  echo -e "\033[41;37mStopped: root is required to run this script.  \033[0m"
+  exit 1
+fi
+
 echo "Updating APT source..."
 sudo apt update
 
