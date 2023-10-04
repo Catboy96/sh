5f151a30e597dc8061e80cf6222352097d7a6ecf
Ralf Ren <me@ralf.ren>
2023-10-04 19:43:46 +0800

    Create elixir-create.sh

---
+++ elixir/elixir-create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name=$1
+repo=$2
+
+mkdir /opt/elixir-data/$name
+
+pushd /opt/elixir-data/$name
+proxychains git clone $repo repo
+mkdir data
+popd
+
+bash elixir-refresh.sh $name
+chmod -R www-data /opt/elixir-data/$name
